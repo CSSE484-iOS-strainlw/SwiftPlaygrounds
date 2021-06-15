0b8e8494d82fbce38018f8d446f03f4b7ec454b3
Loki <45338967+rhit-strainlw@users.noreply.github.com>
2021-06-14 20:48:16 -0400

    Learning the Basics

---
+++ miniExamples/01_Basics.playground/Contents.swift
@@ -0,0 +1,15 @@
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
+
+let scores = [75,52,93,87,41,83]
+var totalPassing = 0
+for score in scores {
+    if score >= 60{
+        totalPassing += 1
+    }
+}
+totalPassing
+print(totalPassing)
+
+print("\(totalPassing) passed out of \(scores.count)")
